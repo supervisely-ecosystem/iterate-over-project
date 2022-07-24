55d30ab677429a6c2526e732631948b55f2982ae
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-07-24 15:06:14 +0300

    Optimized version (#2)
    * wip
    
    * TODO - download_json_batch
    
    * optimization done
    
    * Update launch.json
    
    Co-authored-by: Maxim Kolomeychenko <maxim@supervise.ly>

---
+++ main_optimized.py
@@ -0,0 +1,31 @@
+import os
+import supervisely as sly
+from dotenv import load_dotenv
+
+
+load_dotenv(os.path.expanduser("~/supervisely.env"), verbose=True)
+api = sly.Api.from_env()
+
+project_id = int(os.environ["modal.state.slyProjectId"])
+project = api.project.get_info_by_id(project_id)
+if project is None:
+    raise KeyError(f"Project with ID {project_id} not found in your account")
+print(f"Project info: {project.name} (id={project.id})")
+
+# get project meta - list of annotation classes and tags
+meta_json = api.project.get_meta(project.id)
+project_meta = sly.ProjectMeta.from_json(meta_json)
+print(project_meta)
+
+datasets = api.dataset.get_list(project.id)
+print(f"There are {len(datasets)} datasets in project")
+
+for dataset in datasets:
+    print(f"Dataset {dataset.name} has {dataset.items_count} images")
+    images = api.image.get_list(dataset.id)
+    for batch in sly.batched(images):
+        image_ids = [image.id for image in batch]
+        annotations = api.annotation.download_json_batch(dataset.id, image_ids)
+        for image, ann_json in zip(batch, annotations):
+            ann = sly.Annotation.from_json(ann_json, project_meta)
+            print(f"There are {len(ann.labels)} objects on image {image.name}")
