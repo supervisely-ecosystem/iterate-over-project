46aff92bbb9c6610b317cd45709b9705bdcb8d06
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-08-04 14:29:24 +0300

    Update create_venv.sh

--- create_venv.sh
+++ create_venv.sh
@@ -4,14 +4,14 @@
 # Official python docs: https://docs.python.org/3/library/venv.html
 # Superviely developer portal: https://developer.supervise.ly/getting-started/installation#venv
 
-if [ -d "venv" ]; then
+if [ -d ".venv" ]; then
     echo "VENV already exists, will be removed"
-    rm -rf venv
+    rm -rf .venv
 fi
 
 echo "VENV will be created" && \
-python3 -m venv venv && \
-source venv/bin/activate && \
+python3 -m venv .venv && \
+source .venv/bin/activate && \
 
 echo "Install requirements..." && \
 pip3 install -r requirements.txt && \
