f992a8585a31e2ba5050560305913850fa5756b2
vorozhkog <115161827+vorozhkog@users.noreply.github.com>
2023-03-09 15:19:41 +0000

    renewed code to match latest sdk

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import os
 import supervisely as sly
 from dotenv import load_dotenv
 
-load_dotenv("local.env")
-load_dotenv(os.path.expanduser("~/supervisely.env"))
+if sly.is_development():
+    load_dotenv("local.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api = sly.Api.from_env()
 
-project_id = int(os.environ["modal.state.slyProjectId"])
+project_id = sly.env.project_id()
 project = api.project.get_info_by_id(project_id)
-if project is None:
-    raise KeyError(f"Project with ID {project_id} not found in your account")
+
 print(f"Project info: {project.name} (id={project.id})")
 
 # get project meta - collection of annotation classes and tags
